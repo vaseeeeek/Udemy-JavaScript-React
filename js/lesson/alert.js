cb3b8b20ed3ac745873a24db9a07b7ac69cd7a54
Vasiliy Lapshinkin <vaseeeeek53@gmail.com>
2020-10-23 10:23:58 +0300

    first commit

---
+++ js/lesson/alert.js
@@ -0,0 +1,12 @@
+"use strict";
+
+//alert('хай');
+
+//const result = confirm("ты чо пидар?"); //задать вопрос (true or false)
+
+//console.log(result) 
+
+//const answer = prompt('Вам 18?', '18'); //задать вопрос (string в ответ)
+
+//console.log(typeof(answer)); // answer ответ typeof тип переменной
+
---
+++ js/lesson/obg.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const obg = { // объект
+    name: 'John',
+    age: '25',
+    isMarried: 'false'
+};
+
+console.log(obg.age); // вывод возвраста 
+
+let arr = ['plum.png', 'orange', 6, 'apple.bmp']  //массив
+
+console.log(arr[2])
+
+
+const answers = [];
+
+answers[1] = prompt('Имя');
+answers[2] = prompt('Фамилия');
+answers[3] = prompt('Возвраст');
