3f4c818b17eba062aa03619d4ae1220465aa4944
Vasiliy Lapshinkin <vaseeeeek53@gmail.com>
2020-10-26 17:46:04 +0300

    26.10 job

---
+++ js/lesson/strNum.js
@@ -0,0 +1,22 @@
+// const str = 'test';
+
+// console.log(str[2]) // === S
+
+// console.log(str.toLowerCase) // нижний регистр
+
+// const fruit = 'some fruit';
+
+// console.log(fruit.indexOf("fruit")) // слово fruit находится на 5 символе (позиции)
+
+// const logg ='Hello world';
+
+// console.log(logg.slice(6, 11)); // вытащить слово world элементы с 6 по 11 не велючая, если не указывать второй элемент строка вырозится до конца
+
+// console.log(logg.substring(6, 11)); // вытащить слово world элементы с 6 по 11 не велючая, если не указывать второй элемент строка вырозится до конца
+
+// console.log(logg.substr(6, 5)); // вытащить слово world элементы с 6 5 символов
+
+// const test = "12.2px";
+
+// console.log(parseInt(test)); // 12
+// console.log(parseFloat(test)); // 12.2
--- js/lesson/testJsFilms.js
+++ js/lesson/testJsFilms.js
@@ -1,3 +1,16 @@
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
 'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
@@ -10,16 +23,6 @@ const personalMovieDB = {
     privat: false
 };
 
-if (personalMovieDB.count < 11) {
-    console.log('Просмотрено довольно мало фильмов');
-}   else if (personalMovieDB.count > 10 && personalMovieDB.count < 31 ) {
-    console.log('Вы классический зритель');
-}   else if (personalMovieDB.count > 30 ) {
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла ошибка');
-}
-
 for (let i = 0; i < 2; i++) {
     const lastFilms = prompt('Один из последних просмотренных фильмов?', ''),
           ratingFilms = prompt('На сколько оцените его?', '');
@@ -31,6 +34,16 @@ for (let i = 0; i < 2; i++) {
         console.log('error');
         i--;
     }
+}
+
+if (personalMovieDB.count < 11) {
+    console.log('Просмотрено довольно мало фильмов');
+}   else if (personalMovieDB.count > 10 && personalMovieDB.count < 31 ) {
+    console.log('Вы классический зритель');
+}   else if (personalMovieDB.count > 30 ) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
 }
 
 console.log(personalMovieDB);
